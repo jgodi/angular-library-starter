70c0baa8f55a1368e8555f051ff5c321c60a4fad
Joshua Godi <jgodi@bullhorn.com>
2017-04-15 14:11:19 -0500

    chore(tests): Fixing tests to run (using karma-typescript)

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,8 @@
-/* tslint:disable: max-classes-per-file */
-// NG2
-import { ModuleWithProviders, NgModule } from '@angular/core';
-// APP
-import { MeowModule } from './meow/meow.module';
-
 // Export ALL the things!!
-
 export {
     MeowComponent, MeowModule
 } from './meow';
 
-const MODULES = [
-    MeowModule
-];
-
-@NgModule({
-    imports: [
-        MeowModule.forRoot()
-    ],
-    exports: MODULES
-})
-export class NgLibraryStarterRootModule {
-}
-
-@NgModule({ exports: MODULES })
-export class NgLibraryStarterModule {
-    public static forRoot(): ModuleWithProviders {
-        return { ngModule: NgLibraryStarterRootModule };
-    }
-}
+export {
+    NgLibraryStarterModule, NgLibraryStarterRootModule
+} from './ng-starter.module';
